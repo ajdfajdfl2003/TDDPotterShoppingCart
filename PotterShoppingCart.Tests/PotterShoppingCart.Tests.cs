2b1299cb9ba7280564362c5656690c34d319b368
Angus <ajdfajdfl2003@gmail.com>
2017-01-17 22:12:49 +0800

    init Project

---
+++ PotterShoppingCart.Tests/PotterShoppingCart.Tests.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace PotterShoppingCart.Tests
+{
+    /// <summary>
+    /// PotterShoppingCart 的摘要描述
+    /// </summary>
+    [TestClass]
+    public class PotterShoppingCartTests
+    {
+    }
+}
