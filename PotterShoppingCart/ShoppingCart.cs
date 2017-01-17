ff76b5c3e19bab7e0312b4643a264708f3ff4166
Angus <ajdfajdfl2003@gmail.com>
2017-01-17 22:43:02 +0800

    [Green Light] Add Feature for Second Scenario

--- PotterShoppingCart/ShoppingCart.cs
+++ PotterShoppingCart/ShoppingCart.cs
@@ -7,15 +7,28 @@ namespace PotterShoppingCart
     public class ShoppingCart
     {
         private List<Book> _books;
-        
+        private Dictionary<int, double> _ratioDiscount = new Dictionary<int, double>() {
+            { 1, 1 },
+            { 2, 0.95 }
+        };
+
+
         public ShoppingCart(List<Book> books)
         {
             this._books = books;
         }
 
-        public object CalculateAmount()
+        public double CalculateAmount()
         {
-            return this._books.Sum( x => x.UnitPrice * x.Quantity );
+            var bookCount = this._books.Count(x => x.Quantity > 0);
+
+            if (bookCount == 0)
+            {
+                return 0;
+            }
+
+            var discount = this._ratioDiscount.Count() < bookCount ? 1 : this._ratioDiscount[bookCount];
+            return this._books.Sum(x => x.UnitPrice * x.Quantity) * discount;
         }
     }
 }
