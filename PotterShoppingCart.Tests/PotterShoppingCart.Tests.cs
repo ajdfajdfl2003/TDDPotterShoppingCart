69ba23a7d48a7d078781b1872a9b809b3c8ff97b
Angus <ajdfajdfl2003@gmail.com>
2017-01-18 01:11:53 +0800

    [Green Light] Add Sixth Test Scenario: 第一集買了一本_第二三集各買了兩本_價格應為460元
    感覺有點太慢重構

--- PotterShoppingCart.Tests/PotterShoppingCart.Tests.cs
+++ PotterShoppingCart.Tests/PotterShoppingCart.Tests.cs
@@ -171,5 +171,32 @@ namespace PotterShoppingCart.Tests
             var expected = 370;
             actual.Should().Be(expected);
         }
+
+        [TestMethod]
+        public void 第一集買了一本_第二三集各買了兩本_價格應為460元()
+        {
+            //Scenario: 第一集買了一本，第二三集各買了兩本，價格應為100*3*0.9 + 100*2*0.95 = 460
+            //	Given 第一集買了 1 本
+            //	And 第二集買了 2 本
+            //	And 第三集買了 2 本
+            //	And 第四集買了 0 本
+            //	And 第五集買了 0 本
+            //	When 結帳
+            //	Then 價格應為 460 元
+            var books = new List<Book>
+            {
+                new Book() { Category = "Harry Potter", Episode = "First",  Quantity = 1, UnitPrice = 100},
+                new Book() { Category = "Harry Potter", Episode = "Second",  Quantity = 2, UnitPrice = 100},
+                new Book() { Category = "Harry Potter", Episode = "Third",  Quantity = 2, UnitPrice = 100}
+            };
+            var target = new ShoppingCart(books);
+
+            //act
+            var actual = target.CalculateAmount();
+
+            //assert
+            var expected = 460;
+            actual.Should().Be(expected);
+        }
     }
 }
